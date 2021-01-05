3de7fbeda2fc5f6ab868991d1ccb39f4e5ad5452
Jose Moreno <jomoreno@teksystems.com>
2021-01-05 13:57:18 -0600

    refactored insertion sort

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+def has_duplicate(arr):
+    found_duplicate = False
+    known_nums = {}
+    num_steps = 0
+    for i in range(len(arr) - 1):
+        known_nums.__setitem__(arr[i], arr[i])
+        if known_nums.get(arr[i + 1]):
+            found_duplicate = True
+        num_steps += 1
+    return str(found_duplicate) + ", num of steps " + str(num_steps)
+
+
+arr0 = [3, 2, 1, 4, 2, 5]
+print(has_duplicate(arr0))
+
+################################################################
 arr1 = [7, 3, 4, 1, 2]
 
-# # noinspection SpellCheckingInspection
+
+# noinspection SpellCheckingInspection
 
 
 def bubblesort(arr):
@@ -24,12 +41,12 @@ print(arr1, num1)
 ################################################################
 arr2 = [7, 3, 4, 1, 2]
 
+
 # noinspection SpellCheckingInspection
 
 
 def selectionsort(arr):
     numswaps = 0
-    lowestindex = -1
     for li in range(len(arr)):
         lowestindex = li
         for j in range(li + 1, len(arr)):
@@ -45,20 +62,43 @@ arr2, num2 = selectionsort(arr2)
 print(arr2, num2)
 
 
-# def everyother(array):
-#     counter = 0
-#     length = len(array)
-#     newarr = []
-#     while counter < length:
-#         newarr += array[counter],
-#         counter += 2
-#     print(newarr)
+# noinspection SpellCheckingInspection
+
+def everyother(array):
+    counter = 0
+    length = len(array)
+    newarr = []
+    while counter < length:
+        newarr += array[counter],
+        counter += 2
+    print(newarr)
 
 
-# everyother(arr2)
+everyother(arr2)
 
 ################################################################
 arr3 = [4, 2, 7, 1, 3]
 
 
+# noinspection SpellCheckingInspection
+
+
 def insertionsort(arr):
+    numswaps = 0
+    arrlen = len(arr)
+    for i in range(1, arrlen):
+        temp = arr[i]
+        checkleft = i - 1
+        while checkleft >= 0:
+            if temp < arr[checkleft]:
+                arr[checkleft + 1] = arr[checkleft]
+            else:
+                break
+            checkleft -= 1
+        numswaps += 1
+        arr[checkleft + 1] = temp
+    return arr, numswaps
+
+
+arr3, num3 = insertionsort(arr3)
+print(arr3, num3)
