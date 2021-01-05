0198a29cb6a1b1cdff64adc427fb460fe3107dd7
Jose Moreno <jomoreno@teksystems.com>
2021-01-05 17:57:04 -0600

    refactored sort methods to track comparisons and swaps

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ arr0 = [3, 2, 1, 4, 2, 5]
 print(has_duplicate(arr0))
 
 ################################################################
-arr1 = [7, 3, 4, 1, 2]
+arr1 = [1, 3, 4, 2]
 
 
 # noinspection SpellCheckingInspection
@@ -23,82 +23,73 @@ arr1 = [7, 3, 4, 1, 2]
 def bubblesort(arr):
     dynamiclen = len(arr) - 1
     issorted = False
+    numcomps = 1
     numswaps = 0
     while not issorted:
         issorted = True
         for i in range(dynamiclen):
+            numcomps += 1
             if arr[i] > arr[i + 1]:
                 numswaps += 1
                 issorted = False
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
         dynamiclen -= 1
-    return arr, numswaps
+    return arr, numcomps, numswaps
 
 
-arr1, num1 = bubblesort(arr1)
-print(arr1, num1)
+arr1, num1c, num1s = bubblesort(arr1)
+print(arr1, num1c, num1s)
 
 ################################################################
-arr2 = [7, 3, 4, 1, 2]
+arr2 = [1, 3, 4, 2]
 
 
 # noinspection SpellCheckingInspection
 
 
 def selectionsort(arr):
+    numcomps = 0
     numswaps = 0
     for li in range(len(arr)):
         lowestindex = li
         for j in range(li + 1, len(arr)):
+            numcomps += 1
             if arr[lowestindex] > arr[j]:
                 lowestindex = j
         if li != lowestindex:
             numswaps += 1
             arr[li], arr[lowestindex] = arr[lowestindex], arr[li]
-    return arr, numswaps
+    return arr, numcomps, numswaps
 
 
-arr2, num2 = selectionsort(arr2)
-print(arr2, num2)
-
-
-# noinspection SpellCheckingInspection
-
-def everyother(array):
-    counter = 0
-    length = len(array)
-    newarr = []
-    while counter < length:
-        newarr += array[counter],
-        counter += 2
-    print(newarr)
-
-
-everyother(arr2)
+arr2, num2c, num2s = selectionsort(arr2)
+print(arr2, num2c, num2s)
 
 ################################################################
-arr3 = [4, 2, 7, 1, 3]
+arr3 = [1, 3, 4, 2]
 
 
 # noinspection SpellCheckingInspection
 
 
 def insertionsort(arr):
+    numcomps = 0
     numswaps = 0
     arrlen = len(arr)
-    for i in range(1, arrlen):
-        temp = arr[i]
-        checkleft = i - 1
-        while checkleft >= 0:
-            if temp < arr[checkleft]:
-                arr[checkleft + 1] = arr[checkleft]
+    for i in range(arrlen - 1):
+        position = i + 1
+        temp = arr[position]
+        while position > 0:
+            numcomps += 1
+            if temp < arr[position - 1]:
+                numswaps += 1
+                arr[position] = arr[position - 1]
+                position -= 1
             else:
                 break
-            checkleft -= 1
-        numswaps += 1
-        arr[checkleft + 1] = temp
-    return arr, numswaps
+        arr[position] = temp
+    return arr, numcomps, numswaps
 
 
-arr3, num3 = insertionsort(arr3)
-print(arr3, num3)
+arr3, num3c, num3s = insertionsort(arr3)
+print(arr3, num3c, num3s)
